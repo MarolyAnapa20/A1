08817868a51acfb6a6d038df38c591c343766ecf
Stalin Francis <educaysoft@gmail.com>
2022-10-05 11:05:22 -0500

    nuevos arxhivos

---
+++ suma.cpp
@@ -0,0 +1,17 @@
+//Creado por Añapa Corozo Maroly Mayerli
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+ float AM_x,AM_y,AM_z;
+ cout<<"Ingrese 2 numero x y :";
+ cin>>x>>y;
+ AM_z=AM_x+AM_y;
+ cout<<"El resutado es : ";
+ cout<<z<<endl;
+
+ return 0;
+
+}
